ee049b3f7c020a5717101c3a4940a960d0ef683c
yut23 <yut23@users.noreply.github.com>
2015-08-20 18:51:30 -0400

    Add code

---
+++ ButtonMapperMod/BesiegeModLoader.cs
@@ -0,0 +1,26 @@
+﻿using spaar.ModLoader;
+using System;
+using UnityEngine;
+
+namespace yut23.ButtonMapper
+{
+    public class BesiegeModLoader : Mod
+    {
+        public GameObject temp;
+        public override string Name { get { return "button-mapper-mod"; } }
+        public override string DisplayName { get { return "Button Mapper Mod"; } }
+        public override string Author { get { return "Yut23"; } }
+        public override Version Version { get { return new Version(1, 0); } }
+        public override string BesiegeVersion { get { return "v0.10"; } }
+        public override bool CanBeUnloaded { get { return true; } }
+
+        public override void OnLoad()
+        {
+            GameObject gameObject = new GameObject();
+            gameObject.AddComponent<ButtonMapperMod>();
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+
+        public override void OnUnload() { GameObject.Destroy(temp); }
+    }
+}
---
+++ ButtonMapperMod/ButtonMapperMod.cs
@@ -0,0 +1,46 @@
+﻿using spaar.ModLoader;
+using System;
+using UnityEngine;
+
+namespace yut23.ButtonMapper
+{
+    public class ButtonMapperMod : MonoBehaviour
+    {
+        string translateKey, eraseKey, mapKey;
+        TranslateButton translateButton = null;
+        EraseButton eraseButton = null;
+        KeyMapModeButton mapButton = null;
+
+        public void Start()
+        {
+            OnConfigurationChange(null, null);
+            Configuration.OnConfigurationChange += OnConfigurationChange;
+        }
+
+        void OnConfigurationChange(object sender, ConfigurationEventArgs e)
+        {
+            translateKey = Configuration.GetString("translate", "t");
+            eraseKey = Configuration.GetString("erase", "n");
+            mapKey = Configuration.GetString("keymap", "m");
+        }
+
+        public void Update()
+        {
+            if (Game.AddPiece != null)
+            {
+                if (translateButton == null || eraseButton == null || mapButton == null)
+                {
+                    translateButton = GameObject.FindObjectOfType<TranslateButton>();
+                    eraseButton = GameObject.FindObjectOfType<EraseButton>();
+                    mapButton = GameObject.FindObjectOfType<KeyMapModeButton>();
+                }
+                if (!AddPiece.isSimulating && !LevelEditController.levelEditActive)
+                {
+                    if (Input.GetKeyDown(translateKey)) translateButton.OnMouseDown();
+                    if (Input.GetKeyDown(eraseKey)) eraseButton.OnMouseDown();
+                    if (Input.GetKeyDown(mapKey)) mapButton.OnMouseDown();
+                }
+            }
+        }
+    }
+}
